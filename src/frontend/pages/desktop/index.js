0285a99e44edd33ca6f9b621c07f9509f6cf35e3
Gee Nine <69750024+lkpnchl@users.noreply.github.com>
2024-04-14 18:52:07 +0800

    added task-bar, home icon, log out

--- src/frontend/pages/desktop/index.js
+++ src/frontend/pages/desktop/index.js
@@ -1,13 +1,39 @@
 import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+import {
+  HomeIcon,
+  ArrowRightFromLineIcon,
+} from 'lucide-react'
+import { Button } from '../../components/ui/button'
 import { Label } from '../../components/ui/label'
 import styles from './desktop.module.css'
 
 export const Desktop = ({ }) => {
 
   return (
-    <div className={`${styles.loginContainer} flex flex-col items-center justify-center pb-12 mb-12`}>
+    <>
+    <div className={`${styles.container} flex align-left h-screen`}>
+      <aside className="w-12 bg-gradient drop-shadow-md flex flex-col justify-between">
+        <div className="h-12 flex items-center justify-center border-b-2 border-white-250">
+          <Button variant="link" size="icon">
+            <Link to="/desktop">
+              <HomeIcon className="h-7 w-7 text-white" />
+            </Link>
+          </Button>
+        </div> 
+        <div className="h-12 flex items-center justify-center border-t-2 border-white-250">
+          <Button variant="link" size="icon">
+            <Link to="/">
+              <ArrowRightFromLineIcon className="h-7 w-7 text-white" />
+            </Link>
+          </Button>
+        </div>
+      </aside>
+    </div>
+    <div className={`items-center justify-center pb-12 mb-12`}>
       <Label className={`${styles.welcomeText} pt-4`}> Display Something here</Label>
       <Label className={`${styles.welcomeText} pt-4`}> Desktop Area</Label>
     </div>
+    </>
   )
 }
