d91148183f531fe55393f8ff6966e627493d98aa
tnrbns <tsrubinos@up.edu.ph>
2024-04-15 19:52:15 +0800

    modified electron.js, added splash.html

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,22 @@
 const path = require("path")
-
 const { app, BrowserWindow } = require("electron")
 const url = require("url")
+
+let splashScreen;
+
+function createSplashScreen() {
+  splashScreen = new BrowserWindow({
+    width: 400,
+    height: 300,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true
+  });
+  splashScreen.loadFile('splash.html');
+  splashScreen.on('closed', () => {
+    splashScreen = null;
+  });
+}
 
 function createWindow() {
 	// Create the browser window.
@@ -38,28 +53,35 @@ function createWindow() {
 	// if (!app.isPackaged) {
 	// 	win.webContents.openDevTools()
 	// }
+	win.once('ready-to-show', () => {
+		win.show();
+		if (splashScreen) {
+		  splashScreen.close();
+		}
+	  });
+	
+	win.on('closed', () => {
+		win = null;
+	});
+
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-	createWindow()
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit()
+app.on('ready', () => {
+	createSplashScreen();
+	setTimeout(createWindow, 2000); // Change delay as needed
+  });
+  
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+	  app.quit();
 	}
-})
-
-app.on("activate", () => {
-	// On macOS it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (BrowserWindow.getAllWindows().length === 0) {
-		createWindow()
+  });
+  
+app.on('activate', () => {
+	if (mainWindow === null) {
+	  createWindow();
 	}
-})
+  });
--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
 		setIsLoggedIn(false)
 	};
 	
-
 	return (
 		<>
 			<div className='background'>
