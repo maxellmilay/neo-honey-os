5ffff27fcb3d13676c1ba4c681fbc0cf495c54ee
tnrbns <tsrubinos@up.edu.ph>
2024-04-15 21:07:16 +0800

    fixed splash screen loading

--- public/electron.js
+++ public/electron.js
@@ -7,15 +7,13 @@ let splashScreen;
 
 function createSplashScreen() {
   splashScreen = new BrowserWindow({
-    width: 500,
-    height: 500,
+    width: 400,
+    height: 300,
     frame: false,
     // transparent: true,
     alwaysOnTop: true
   });
-//   splashScreen.loadFile('splash.html');
-
-
+  
 	splashScreen.loadURL(
 		url.format({
 		pathname: path.join(__dirname, 'splash.html'),
@@ -23,8 +21,6 @@ function createSplashScreen() {
 		slashes: true
 		})
 	);
-
-//   splashScreen.loadURL(appURL)
 
   splashScreen.on('closed', () => {
     splashScreen = null;
@@ -40,7 +36,7 @@ function createWindow() {
 		center: true,
 		resizable: false,
 		maximizable: true,
-		alwaysOnTop: true,
+		show: false, // Hide the window by default
 		webPreferences: {
 			preload: path.join(__dirname, "../src/backend/controllers/preload.js"),
 			nodeIntegration: true,
@@ -49,11 +45,7 @@ function createWindow() {
 		},
 	})
 
-	// Set minimum window size
-	// mainWindow.setMinimumSize(700, 650)
-
-	// and load the index.html of the app.
-	// mainWindow.loadFile("index.html");
+	// Load the index.html of the app.
 	const appURL = app.isPackaged
 		? url.format({
 				pathname: path.join(__dirname, "index.html"),
@@ -61,23 +53,20 @@ function createWindow() {
 				slashes: true,
 		  })
 		: "http://localhost:3000"
+		
 	mainWindow.loadURL(appURL)
 
-	// Open the DevTools.
-	// if (!app.isPackaged) {
-	// 	mainWindow.webContents.openDevTools()
-	// }
 	mainWindow.once('ready-to-show', () => {
+		// Show the window only when all assets are loaded
 		mainWindow.show();
 		if (splashScreen) {
-		  splashScreen.close();
+			splashScreen.close();
 		}
-	  });
-	
+	});
+
 	mainWindow.on('closed', () => {
 		mainWindow = null;
 	});
-
 }
 
 // This method will be called when Electron has finished
@@ -85,8 +74,8 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
 	createSplashScreen();
-	setTimeout(createWindow,5000); // Change delay as needed
-  });
+	setTimeout(createWindow, 5000); // Change delay as needed
+});
   
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
@@ -94,8 +83,8 @@ app.on('window-all-closed', () => {
 	}
   });
   
-  app.on('activate', () => {
-	if (mainWindow === null) {
-	  createWindow();
-	}
-  });
+app.on('activate', () => {
+if (mainWindow === null) {
+	createWindow();
+}
+});
