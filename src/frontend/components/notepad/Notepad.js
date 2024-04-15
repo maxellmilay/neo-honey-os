32005992db9629d56564a653e9b92c09a20bb06a
mdelacroix <mldelacruz8@up.edu.ph>
2024-04-16 00:18:16 +0800

    delete unnecessary

--- src/frontend/components/notepad/Notepad.js
+++ src/frontend/components/notepad/Notepad.js
@@ -68,23 +68,6 @@ function Notepad() {
             </div>
           </DialogClose>
         </div>
-
-        <div className="flex items-center space-x-2">
-          <div className="grid flex-1 gap-2">
-            <Label htmlFor="link" className="sr-only">
-              Link
-            </Label>
-            <Input
-              id="link"
-              defaultValue="https://ui.shadcn.com/docs/installation"
-              readOnly
-            />
-          </div>
-          <Button type="submit" size="sm" className="px-3">
-            <span className="sr-only">Copy</span>
-            <Copy className="h-4 w-4" />
-          </Button>
-        </div>
         <DialogFooter className="sm:justify-start">
           <DialogClose asChild>
             <Button type="button" variant="secondary">
