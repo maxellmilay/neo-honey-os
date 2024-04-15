b44ffa5d056bf91cb5cf31d1fd2a24d2df8a9916
Clelia Cyra Molanda <caseycyra@gmail.com>
2024-04-16 02:04:40 +0800

    onclick from lockscreen

--- src/frontend/pages/boot/index.js
+++ src/frontend/pages/boot/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'
 import { Label } from '../../components/ui/label';
 import styles from './boot.module.css'; // Import CSS styles
 
+
 export const BootApp = () => {
+  const history = useNavigate();
+
+  const nextPage = async () => {
+    history('/desktop')
+  }
+
   return (
-    <div className={styles.bodyBackground}> {/* Apply background class */}
-      <Label className={styles.welcomeText}>Welcome Honey!</Label> {/* Apply welcomeText class */}
+    <div className={styles.bodyBackground} onClick={nextPage}> {/* Apply background class */}
+      <Label className={styles.welcomeText}>Welcome, Honey!</Label> {/* Apply welcomeText class */}
     </div>
   );
 };
