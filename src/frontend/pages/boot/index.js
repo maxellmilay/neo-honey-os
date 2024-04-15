b9261d38f575838bd9480d9ac533f8691d59f40d
Clelia Cyra Molanda <caseycyra@gmail.com>
2024-04-16 03:16:37 +0800

    lockscreen with time and date

--- src/frontend/pages/boot/index.js
+++ src/frontend/pages/boot/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { Label } from '../../components/ui/label';
 import styles from './boot.module.css'; // Import CSS styles
@@ -7,13 +7,30 @@ import styles from './boot.module.css'; // Import CSS styles
 export const BootApp = () => {
   const history = useNavigate();
 
+  const [currentTime, setCurrentTime] = useState('');
+  const [currentDate, setCurrentDate] = useState('');
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const timeString = now.toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit' });
+      const options = { weekday: 'long', month: 'long', day: 'numeric' };
+      const dateString = now.toLocaleDateString('en-US', options);
+      setCurrentTime(timeString);
+      setCurrentDate(dateString);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   const nextPage = async () => {
     history('/desktop')
   }
 
   return (
-    <div className={styles.bodyBackground} onClick={nextPage}> {/* Apply background class */}
-      <Label className={styles.welcomeText}>Welcome, Honey!</Label> {/* Apply welcomeText class */}
+    <div className={`${styles.bodyBackground} `} onClick={nextPage}>
+      <Label className={styles.currentTime}>{currentTime}</Label>
+      <Label className={styles.currentDate}>{currentDate}</Label>
     </div>
   );
 };
