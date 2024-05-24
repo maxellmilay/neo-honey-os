2da360180ae5167e9393944c047ee2ed02729f97
mdelacroix <mldelacruz8@up.edu.ph>
2024-05-24 08:44:10 +0800

    update header no clock

--- src/frontend/components/pcb/component/header.jsx
+++ src/frontend/components/pcb/component/header.jsx
@@ -13,12 +13,7 @@ export default class Header extends Component {
                     </div>
                     <input type="text" name="referenceInputTextField" className="form-control" placeholder=" Job Process [0-9] separated with ','" value={referenceInputTextField} onChange={handleRefChange.bind(this)} />
                 </div>
-                <div className="input-group mt-2">
-                    <div className="input-group-prepend">
-                        <span className="input-group-text" id="addon-wrapping" data-toggle="tooltip" data-placement="top" title="For NRU, enter reset clock 0~9">Reset Clock</span>
-                    </div>
-                    <input ref="resetTurns" type="number" min="0" max="9" className="form-control" placeholder="Number of reset turns for NRU [0-9]" value={resetTurns} onChange={handleResetTurnsChange.bind(this)} />
-                </div>
+            
                 <button type="button" className={"btn btn-danger mt-2 mr-2"} onClick={handleRefStringGenClick}>Generate String</button>
             </div>
         )
