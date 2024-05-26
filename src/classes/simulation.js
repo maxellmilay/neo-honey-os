f438a240c2f75da159182c2343916a5833818007
tnrbns <tsrubinos@up.edu.ph>
2024-05-26 20:36:12 +0800

    processes sorted

--- src/classes/simulation.js
+++ src/classes/simulation.js
@@ -2,6 +2,7 @@ import { Algorithm } from './algorithm';
 import { Job } from './job';
 
 export class Simulation {
+
   constructor(algorithm, jobs) {
     this.time = 0;
     this.algorithm = algorithm;
@@ -10,6 +11,9 @@ export class Simulation {
     this.currentJob = undefined;
     this.ganttChart = [];
     this.idleTime = 0;
+
+    // Initialize all jobs with status "New"
+    this.jobs.forEach(job => job.setStatus("New"));
   }
 
   nextStep() {
@@ -29,9 +33,17 @@ export class Simulation {
     newJobs.forEach(job => {
       if (!this.readyQueue.includes(job)) {
         this.readyQueue.push(job);
+        job.setStatus("New"); // Job is "New" when it enters the readyQueue
       }
     });
-    
+
+    if (this.currentJob && this.currentJob.remaining === 1) {
+      const nextJob = this.readyQueue[0];
+      if (nextJob && nextJob.status === "New") {
+        nextJob.setStatus("Ready");
+      }
+    }
+
     this.algorithm.processQueue(this.readyQueue, this.currentJob);
 
     this.currentJob = this.readyQueue.shift();
@@ -43,27 +55,47 @@ export class Simulation {
       this.idleTime += this.currentJob ? 0 : 1;
     }
 
-    this.jobs.forEach(job => {
-      if (job.remaining === 0) {
-          job.setStatus("Terminated");
-      } else if (this.currentJob === job) {
-          job.setStatus("Running");
-      } else if (this.readyQueue.includes(job)) {
-          job.setStatus("Waiting"); // Changed from "Ready" to "Waiting"
-      } else {
-          job.setStatus("Ready"); // Changed from "New" to "Ready"
-      }
-      // Add more conditions here if needed
-  });
-
+    this.updateJobStatuses();
   }
 
   addNewJob() {
     const newJobId = this.jobs.length + 1;
     const newJob = Job.createRandomJob(newJobId);
     newJob.arrivalTime = this.time; // Set arrivalTime to the current time
+    newJob.setStatus("New");
     this.jobs.push(newJob);
     this.readyQueue.push(newJob);
+  }
+
+  updateJobStatuses() {
+    this.jobs.forEach(job => {
+      if (job.remaining === 0) {
+          job.setStatus("Terminated");
+      } else if (this.currentJob === job) {
+          job.setStatus("Running");
+      } else if (job === this.readyQueue[0]) { // Check if the job is the first in the ready queue
+          job.setStatus("Ready");
+      } else if (job.status === "New" && this.time - job.arrivalTime >= 1) {
+          job.setStatus("Waiting");
+      } else if (job.status === "New") {
+          // Keep the status as "New" if less than 1 unit of time has elapsed
+          job.setStatus("New");
+      } else {
+          job.setStatus("Waiting");
+      }
+  });
+  
+    const statusOrder = {
+      'Running': 1,
+      'Ready': 2,
+      'New': 3,
+      'Waiting': 4,
+      'Terminated': 5
+    };
+
+    this.jobs.sort((a, b) => {
+      return statusOrder[a.status] - statusOrder[b.status];
+    });
   }
 
   reset() {
@@ -73,6 +105,9 @@ export class Simulation {
     this.currentJob = undefined;
     this.ganttChart = [];
     this.idleTime = 0;
+
+    // Reset all job statuses to "New"
+    this.jobs.forEach(job => job.setStatus("New"));
   }
 
   isFinished() {
