81bc33fc820438b50b875a8fb9c95fca75435d3a
tnrbns <tsrubinos@up.edu.ph>
2024-05-27 20:50:46 +0800

    table for prio queue same css

--- src/frontend/components/jobPoolTable/index.js
+++ src/frontend/components/jobPoolTable/index.js
@@ -46,7 +46,7 @@ function JobPoolTable({ simulation, selectedAlgo }) {
                 
                 <Table className="w-full h-full"> 
                     <ScrollArea className="h-[25rem] w-auto p-2">
-                    <TableHeader className="sticky z-50 top-0 bg-slate-100 drop-shadow-sm">
+                    <TableHeader className="sticky z-50 top-0 bg-[#FEF8D8] drop-shadow-sm">
                     <TableRow>
                             <TableHead className="text-center font-bold">Process ID</TableHead>
                             <TableHead className="text-center font-bold">Arrival</TableHead>
@@ -63,7 +63,7 @@ function JobPoolTable({ simulation, selectedAlgo }) {
                             return a.id - b.id;
                         })
                         .map((item, index) => (
-                            <TableRow key={index} className={index === 0 ? "font-bold bg-white" : ""}>
+                            <TableRow key={index} className={index === 0 ? "sticky z-49 top-[35.5px] font-bold text-amber-950 bg-yellow-300" : ""}>
                                 <TableCell>{item.id}</TableCell>
                                 <TableCell>{item.arrivalTime}</TableCell>
                                 <TableCell>{item.burst}</TableCell>
