2a3024a4dc5001230ab1d70562851e0f0bf3bf53
mdelacroix <mldelacruz8@up.edu.ph>
2024-05-27 19:37:45 +0800

    add dummy data memory and update memory management

---
+++ src/frontend/components/pcb/dummydatamem.js
@@ -0,0 +1,8 @@
+const processes = [
+  { processId: 1, processTime: 2, processSize: 2 },
+  { processId: 2, processTime: 2, processSize: 2 },
+  { processId: 3, processTime: 2, processSize: 2 },
+  { processId: 4, processTime: 2, processSize: 2 },
+];
+
+export default processes;
